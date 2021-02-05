5d4b3a1b5204b121ffaf6236a114bceb4e11f0b6
Ragul-bfx <75634790+Ragul-bfx@users.noreply.github.com>
2021-02-05 20:13:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!!Running Successfully\n'
+  const msg = 'Hello Node!!Running Successfully and the code is good\n'
   res.end(msg);
 });
 
